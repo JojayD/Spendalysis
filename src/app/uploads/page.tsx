0c73f5759b888c1d0b7fd02128f394d52e615010
JojayD <professional.josephd@gmail.com>
2025-01-09 01:43:42 -0800

    Add dashboard, dark mode toggle, and update branding

---
+++ src/app/uploads/page.tsx
@@ -0,0 +1,12 @@
+import {InputFile} from "@/components/file";
+export default function Upload(){
+
+    return(
+
+        <div>
+            <label>Upload a CSV</label>
+            <InputFile/>
+        </div>
+
+    );
+}
